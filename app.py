5d27ec6b620f7f6a30514fb23f1fbb0b740a6ba6
GRamaHarika <65611744+GRamaHarika@users.noreply.github.com>
2020-05-21 18:40:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 21 16:13:13 2020
+
+@author: hp1
+"""
+
+
+import numpy as np
+import os
+from keras.models import load_model
+from keras.preprocessing import image
+import tensorflow as tf
+global graph
+graph = tf.get_default_graph()
+from flask import Flask , request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+app = Flask(__name__)
+model = load_model(r"C:\Users\hp1\Desktop\Flask\diseases")
+                 
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/predict',methods = ['GET','POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['image']
+        print("current path")
+        basepath = os.path.dirname(__file__)
+        print("current path", basepath)
+        filepath = os.path.join(basepath,'uploads',f.filename)
+        print("upload folder is ", filepath)
+        f.save(filepath)
+        img = image.load_img(filepath,target_size = (64,64)) 
+        x = image.img_to_array(img)
+        x = np.expand_dims(x,axis =0)
+        with graph.as_default():
+            preds = model.predict_classes(x)
+            6
+            print("prediction",preds)
+        index = ['Actinic keratosis','Basal cell carcinoma','Benign','Dermatofibroma','Melanocytic nevi','Melanoma']
+        text = "The Predicted skin disease is : " + str(index[preds[0]])
+    return text
+if __name__ == '__main__':
+    app.run(debug = True, threaded = False)
+    
